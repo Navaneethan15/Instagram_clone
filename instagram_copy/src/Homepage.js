853d7fc98633a827e74d15540bf9fdeddbcd5704
Navaneethan15 <138652808+Navaneethan15@users.noreply.github.com>
2023-11-24 16:27:58 +0530

    insta clone
    Next One

---
+++ instagram_copy/src/Homepage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+   <>
+   <div className='homepage'>
+
+    <div className='homepage_nav' >
+      
+    </div>
+
+
+   </div>
+
+   </>
+  )
+}
+
+
+export default Homepage
